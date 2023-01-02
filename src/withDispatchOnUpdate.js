15eeb9cfde2b4de35f924289c7ff672b9d6e1f5f
jsifalda <sifalda.jiri@gmail.com>
2023-01-02 16:49:24 +0100

    chore: update is-plain-object to v5

--- src/withDispatchOnUpdate.js
+++ src/withDispatchOnUpdate.js
@@ -1,31 +1,26 @@
-import isPlainObject from 'is-plain-object'
-import { connect } from 'react-redux'
-import map from 'ramda/src/map'
+import { isPlainObject } from 'is-plain-object'
 import all from 'ramda/src/all'
-import pipe from 'ramda/src/pipe'
-import not from 'ramda/src/not'
 import isNil from 'ramda/src/isNil'
+import map from 'ramda/src/map'
+import not from 'ramda/src/not'
+import pipe from 'ramda/src/pipe'
 import reject from 'ramda/src/reject'
+import { connect } from 'react-redux'
 
-const isFunction = value => {
+const isFunction = (value) => {
   return typeof value === 'function'
 }
 
 const hasRequiredProps = (props, keys) => {
   return pipe(
     reject(isFunction),
-    map(prop => props[prop]),
-    all(
-      pipe(
-        isNil,
-        not,
-      ),
-    ),
+    map((prop) => props[prop]),
+    all(pipe(isNil, not)),
   )(keys)
 }
 
 const getArgValues = (props, keys) => {
-  return map(prop => {
+  return map((prop) => {
     return isFunction(prop) ? prop(props) : props[prop]
   }, keys)
 }
@@ -41,18 +36,13 @@ const withDispatchOnUpdate = ({
 }) => {
   return connect(
     (state, props) => ({
-      [CONNECTED_PROPS]: isPlainObject(connector)
-        ? map(f => f(state, props), connector)
-        : connector,
+      [CONNECTED_PROPS]: isPlainObject(connector) ? map((f) => f(state, props), connector) : connector,
       state,
     }),
     { boundAction: action },
     ({ [CONNECTED_PROPS]: connectedProps, state }, { boundAction }, props) => {
       const mergedProps = { ...props, ...connectedProps }
-      if (
-        (!shouldRequireAllProps || hasRequiredProps(mergedProps, args)) &&
-        condition(state, mergedProps)
-      ) {
+      if ((!shouldRequireAllProps || hasRequiredProps(mergedProps, args)) && condition(state, mergedProps)) {
         boundAction(...getArgValues(mergedProps, args))
       }
 
